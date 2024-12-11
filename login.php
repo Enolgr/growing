a6c3c307fe06cbb3b37f677c316a6021da62a1aa
Douglas <malfidos30@gmail.com>
2024-12-11 14:04:54 +0100

    Creación formulario login

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <section>
+        <div>
+            <form action="login.php" method="post">
+                <input type="text" id="email" name="email" placeholder="Email">
+                <input type="password" id="pass" name="pass" placeholder="Contraseña">
+                <button type="submit">Iniciar sesión</button>
+            </form>
+            <button type="button">Registrarse</button>
+        </div>
+    </section>
+</body>
+</html>
