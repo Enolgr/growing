a577c6c96181a1ccdb82fa1193d8432ae7d7252d
Douglas <malfidos30@gmail.com>
2024-12-18 14:03:23 +0100

    Creación reproductor.php

--- login.php
+++ login.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- CSS & normalize -->
+     <link rel="stylesheet" href="./styles/normalize.css">
+     <link rel="stylesheet" href="./styles/general.css">
 </head>
 <body>
     <section>
---
+++ reproductor.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reproductor</title>
+
+    <!-- CSS & normalize -->
+     <link rel="stylesheet" href="./styles/normalize.css">
+     <link rel="stylesheet" href="./styles/general.css">
+</head>
+<body>
+    <section>
+        <div class="reproductor">
+            <div class="tituloReproductor">
+                <h1>Reproductor</h1>
+            </div>
+            <div class="video">
+                <video src="*.mp4"></video>
+            </div>
+        </div>
+        <div class="capitulos">
+            <div class="tituloCapitulos">
+                <h1>Capítulos</h1>
+            </div>
+            <div>
+                <ul class="listaCapitulos">
+                    <li><a href="*.mp4">Capitulo 1</a></li>
+                    <li><a href="*.mp4">Capitulo 2</a></li>
+                    <li><a href="*.mp4">Capitulo 3</a></li>
+                    <li><a href="...">...</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
