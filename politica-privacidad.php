dab049d4fce0e5edead9bb4c93468a02d31cf4b0
Carla <carluchi012002@gmail.com>
2024-12-18 13:14:46 +0100

    añado estilos a las politicas y creo archivo descripcion-curso.php

--- politica-privacidad.php
+++ politica-privacidad.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Política de Privacidad</title>
+    <link rel="stylesheet" href="./styles/normalize.css">
+    <link rel="stylesheet" href="./styles/general.css">
   </head>
   <body>
     <header>
