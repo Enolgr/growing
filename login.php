aebe6f47c3a3aa57104ce0224666a06f9c300a90
Enol <enolgr@gmail.com>
2024-12-18 14:17:22 +0100

    cambios en css

--- login.php
+++ login.php
@@ -18,10 +18,8 @@
 
             <form action="login.php" class="card-oscura" method="post">
             <h1 class="titulo-card">Login</h1>
-                <div>
                     <input type="text" id="email" name="email" placeholder="Email" required>
                     <input type="password" id="pass" name="pass" placeholder="Contraseña" required>
-                    <div>
                         <button type="submit" class="btn">Iniciar sesión</button>
                         <a href="registrarse.php">Registrarse</a>
                     </div>
