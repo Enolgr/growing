1999273d114272cda63cf6176c4c42b27d16aaef
Carla <carluchi012002@gmail.com>
2025-01-08 13:26:39 +0100

    html de la pagina de cursos acabado

--- cursos.php
+++ cursos.php
@@ -6,14 +6,53 @@
     <link rel="stylesheet" href="./styles/normalize.css">
     <link rel="stylesheet" href="./styles/general.css">
     <title>Cursos</title>
-
 </head>
-<body>
-    <?php include_once "./includes/menu.php"?>
 
+<body>
     <h1>Nuestros cursos online</h1>
+
     <div class="cursos">
         <div class="detalles-curso">
+            <div class="img-curso">
+                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+            </div>
+            <h2>Titulo curso</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>         
+            <button>Boton</button>
+            <div class="icons">
+                <img src="./img/icons/Fav.svg" alt="favoritos">
+            </div>
+        </div>
+        
+        <div class="detalles-curso">
+            <div class="img-curso">
+                <img src="./img/images/pexels-gary-barnes-6231819.webp" alt="curso tijeras">
+            </div>
+            <h2>Titulo curso</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>         
+            <button>Boton</button>
+            <div class="icons">
+                <img src="./img/icons/Fav.svg" alt="favoritos">
+            </div>
+        </div>  
+
+        <div class="detalles-curso">
+            <div class="img-curso">
+                <img src="./img/images/pexels-gary-barnes-6231990.webp" alt="curso regadera">
+            </div>
+            <h2>Titulo curso</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>         
+            <button>Boton</button>
+            <div class="icons">
+                <img src="./img/icons/Fav.svg" alt="favoritos">
+            </div>
+        </div> 
+        
+        
+        <div class="detalles-curso">
+            <div class="img-curso">
+                <img src="./img/images/pexels-ketut-subiyanto-4132369.webp" alt="curso romero">
+            </div>
             <h2>Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>         
             <button>Boton</button>
@@ -22,54 +61,39 @@
             </div>
         </div>
 
-        <div class="detalles-curso">
-            <h2>Titulo curso</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>
-            <button>Boton</button>
-            <div class="icons">
-                <img src="./img/icons/Fav.svg" alt="favoritos">
-            </div>
-        </div>
-
-        <div class="detalles-curso">
-            <h2>Titulo curso</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>
-            <button>Boton</button>
-            <div class="icons">
-                <img src="./img/icons/Fav.svg" alt="favoritos">
-            </div>
-        </div>
-
-        <div class="detalles-curso">
-            <h2>Titulo curso</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>
-            <button>Boton</button>
-            <div class="icons">
-                <img src="./img/icons/Fav.svg" alt="favoritos">
-            </div>
-        </div>
-
-        <div class="detalles-curso">
-            <h2>Titulo curso</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>
-            <button>Boton</button>
-            <div class="icons">
-                <img src="./img/icons/Fav.svg" alt="favoritos">
-            </div>
-        </div>
 
         <div class="detalles-curso">
             <div class="img-curso">
-                
+                <img src="./img/images/pexels-sasha-kim-9413757.webp" alt="curso plantar">
             </div>
             <h2>Titulo curso</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>         
             <button>Boton</button>
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
             </div>
         </div>
+
+
+        <div class="detalles-curso">
+            <div class="img-curso">
+                <img src="./img/images/pexels-teona-swift-6912856.webp" alt="curso cactus">
+            </div>
+            <h2>Titulo curso</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem. Blanditiis eligendi quaerat excepturi?</p>         
+            <button>Boton</button>
+            <div class="icons">
+                <img src="./img/icons/Fav.svg" alt="favoritos">
+            </div>
+        </div>
+          
     </div>
+    
+    <!-- Aqui iria la paginación de los cursos  -->
+
+    <!-- Aqui iria el hover de suscripción  la newsletter -->
+
+    <!-- Footer -->
     
 </body>
 </html>
