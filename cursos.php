beb2d4c49eae4035a89d11ff2750c8f5da1b2c83
Carla <carluchi012002@gmail.com>
2025-01-15 13:43:19 +0100

    arreglando el ancho del footer de mis paginas

--- cursos.php
+++ cursos.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Cursos</title>
-    <!-- Orden de hojas de estilo -->
+    <!-- Normalize & CSS -->
     <link rel="stylesheet" href="./styles/normalize.css">
     <link rel="stylesheet" href="./styles/general.css">
     <link rel="stylesheet" href="./styles/cursos.css">
@@ -16,19 +16,16 @@
     <!-- Información cursos -->
     <section class="cursos">
         <h1>Nuestros cursos online</h1>
-
-        <div class="detalles-curso" id="detalles-curso">
+        <div class="detalles-curso">
             <div class="img-curso">
                 <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
             </div>
-            <h2>Titulo curso</h2>
+            <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
                 consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
                 Blanditiis eligendi quaerat excepturi?</p>
-
             <!-- Redirigir a la pagina de detalles del curso -->
-            <a href=""><button class="boton">Ver curso</button></a>
-
+            <a href="#"><button class="boton" id="boton">Ver curso</button></a>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
@@ -37,17 +34,13 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <img src="./img/images/pexels-gary-barnes-6231819.webp" alt="curso tijeras">
             </div>
-            <h2>Titulo curso</h2>
+            <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
                 consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
                 Blanditiis eligendi quaerat excepturi?</p>
-
-            <!-- Redirigir a la pagina de detalles del curso -->
-            <a href=""><button class="boton">Ver curso</button></a>
-
-            <!-- Icono para añadir a favoritos -->
+            <a href="#"><button class="boton" id="boton">Ver curso</button></a>
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
             </div>
@@ -55,17 +48,13 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <img src="./img/images/pexels-gary-barnes-6231990.webp" alt="curso regadera">
             </div>
-            <h2>Titulo curso</h2>
+            <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
                 consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
                 Blanditiis eligendi quaerat excepturi?</p>
-
-            <!-- Redirigir a la pagina de detalles del curso -->
-            <a href=""><button class="boton">Ver curso</button></a>
-
-            <!-- Icono para añadir a favoritos -->
+            <a href="#"><button class="boton" id="boton">Ver curso</button></a>
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
             </div>
@@ -73,17 +62,13 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <img src="./img/images/pexels-ketut-subiyanto-4132369.webp" alt="curso romero">
             </div>
-            <h2>Titulo curso</h2>
+            <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
                 consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
                 Blanditiis eligendi quaerat excepturi?</p>
-
-            <!-- Redirigir a la pagina de detalles del curso -->
-            <a href=""><button class="boton">Ver curso</button></a>
-
-            <!-- Icono para añadir a favoritos -->
+            <a href="#"><button class="boton" id="boton">Ver curso</button></a>
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
             </div>
@@ -92,35 +77,28 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <img src="./img/images/pexels-sasha-kim-9413757.webp" alt="curso plantar">
             </div>
-            <h2>Titulo curso</h2>
+            <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
                 consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
                 Blanditiis eligendi quaerat excepturi?</p>
-
-            <!-- Redirigir a la pagina de detalles del curso -->
-            <a href=""><button class="boton">Ver curso</button></a>
-
-            <!-- Icono para añadir a favoritos -->
+            <a href="#"><button class="boton" id="boton">Ver curso</button></a>
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
             </div>
         </div>
 
+
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <img src="./img/images/pexels-teona-swift-6912856.webp" alt="curso cactus">
             </div>
-            <h2>Titulo curso</h2>
+            <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
                 consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
                 Blanditiis eligendi quaerat excepturi?</p>
-
-            <!-- Redirigir a la pagina de detalles del curso -->
-            <a href=""><button class="boton">Ver curso</button></a>
-
-            <!-- Icono para añadir a favoritos -->
+            <a href="#"><button class="boton" id="boton">Ver curso</button></a>
             <div class="icons">
                 <img src="./img/icons/Fav.svg" alt="favoritos">
             </div>
@@ -128,8 +106,6 @@
     </section>
 
     <!-- Aqui iria la paginación de los cursos  -->
-
-
     <!-- Newsletter -->
     <?php include_once "./includes/newsletter.php"; ?>
 
@@ -137,4 +113,5 @@
     <?php include_once "./includes/marquee.php";
     "./includes/footer.php"; ?>
 </body>
+
 </html>
