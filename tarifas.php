65f7861fc8fc7ace6384dd04731d6689e982ba14
Enol <enolgr@gmail.com>
2025-01-15 13:25:11 +0100

    cambios produccion tarifas

--- tarifas.php
+++ tarifas.php
@@ -172,27 +172,11 @@
 
 </section>
 
-<section class="contenedor_form">
 
-<div class="form">
-<h1>Únete a nuestra newsletter</h1>
+<?php
 
-<form action="">
-    
-<div class="campos">
-    <input type="text" class="nombre">
-    <input type="text" class="email">
-</div>
-
-<button class="enviar" >Enviar</button>
-
-</form>
-
-</div>
-
-</section>
-
-<?php include("./includes/marquee.php") ?>
+include "./includes/newsletter.php";
+include "./includes/marquee.php" ?>
 
 </body>
 </html>
