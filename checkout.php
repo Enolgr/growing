c4c94ac9ce00c172efc403d7321e6e30c78f5615
Fidel <fidellauheinz@gmail.com>
2025-02-02 21:49:25 +0100

    gestion de radios de envio y pagos

--- checkout.php
+++ checkout.php
@@ -43,40 +43,78 @@
 
                 <!-- Apartdo  empresa envío -->
                 <div class="deliveryMethod">
+                    <hr>
                     <h2>Método de envío</h2>
-                    <input type="radio" id="delivery" name="delivery" value="glsEcomomy"><p>GLS Economy</p></input>
-                    <p>Entrega en 48-72h</p>
-                    <input type="radio" id="delivery" name="delivery" value="correosExpress"><p>Correos Express</p></input>
-                    <p>Entrega en 24h</p>
+                    <div class="deliveryData">
+                        <div class="radius">
+                            <input type="radio" id="delivery1" name="delivery" value="glsEcomomy"></input>
+                            <input type="radio" id="delivery2" name="delivery" value="correosExpress"></input>
+                        </div>
+                        <div class="dataRadius">
+                            <label for="delivery1">GLS Economy</label>
+                            <p class="subtitle">Entrega en 48-72h</p>
+                            <label for="delivery2">Correos Express</label>
+                            <p class="subtitle">Entrega en 24h</p>
+                        </div>
+                        
+                    </div>
                 </div>
-
                 <!-- Apartado de método de pago -->
                 <div class="payMethod">
+                    <hr>
                     <h2>Pago</h2>
                     <p>Todas las transacciónes son seguras y están encriptadas</p>
-                    <input type="radio" id="payment" name="payment">Tarjeta de crédito</input>
-                    <img src="#">
-                    <input type="text" id="cardNum" placeholder="Número de la tarjeta">
-                    <div class="dateCode">
-                        <input type="text" id="expirationDate" placeholder="Fecha de vencimiento (MM / AA)">
-                        <input type="text" id="secureCode" placeholder="Código de seguridad">
+                    <div class="creditCardContainer">
+                        <div class="radiusAndCreditContainer">
+                            <div class="Radius">
+                                <input type="radio" id="creditCard" name="payment"></input>
+                            </div>
+                            <label for="bizum">Tarjeta de crédito</label>
+                        </div>
+                        
+                        <img src="./img/logos/visa-classic-svgrepo-com.svg" id="visaPayImg" alt="Pago con Visa">
+                        <img src="./img/logos/mastercard-full-svgrepo-com.svg" id="MasterCardPayImg" alt="Pago con Master Card">
+                        <input type="text" id="cardNum" placeholder="Número de la tarjeta">
+                        <div class="dateCode">
+                            <input type="text" id="expirationDate" placeholder="Fecha de vencimiento (MM / AA)">
+                            <input type="text" id="secureCode" placeholder="Código de seguridad">
+                        </div>
+                        <input type="text" id="ownerName" placeholder="Nombre del titular">
+                        <div class="check-checkout">
+                            <?php include "./includes/checkbox.php";?>
+                            <p>Usar la dirección de envío como la dirección de facturación</p>
+                        </div>
                     </div>
-                    <input type="text" id="ownerName" placeholder="Nombre del titular">
-                    <div class="check-checkout">
-                        <?php include "./includes/checkbox.php"; ?>
-                        <p>Usar la dirección de envío como la dirección de facturación</p>
+                    
+                    <div class="otherMethodsContainer">
+                        <div class="Radius">
+                            <input type="radio" id="bizum" name="payment"></input>
+                            <input type="radio" id="googleApple" name="payment"></input>
+                            <input type="radio" id="payPal" name="payment"></input>
+                        </div>
+                        <div class="payText">
+                            <label for="bizum">Pagar con Bizum</label>
+                            <label for="googleApple">Google Pay y Apple Pay</label>
+                            <label for="payPal">PayPal</label>
+                        </div>
+                        <div class="imgsContainer">
+                            <img src="./img/logos/Bizum.svg" id="bizumImg" name="payment" alt="Pago con Bizum">
+                            <div class="appleGoogle">
+                                <img src="./img/logos/google-pay-svgrepo-com.svg" id="googlePayImg" alt="Pago con Google">
+                                <img src="./img/logos/apple-pay-svgrepo-com.svg" id="applePayImg" alt="Pago con Apple Pay">
+                            </div>
+                            <img src="./img/logos/paypal.svg" id="paypalImg" alt="Pago con PayPal ">
+                        </div>
                     </div>
-                    <input type="radio" id="payment" name="payment"><p>Pagar con Bizum</p></input>
-                    <img src="#" id="bizumImg" name="payment">
-                    <input type="radio" id="payment" name="payment"><p>Google Pay y Apple Pay</p></input>
-                    <img src="#" id="googlePayImg">
-                    <img src="#" id="applePayImg">
-                    <input type="radio" id="payment" name="payment"><p>PayPal</p></input>
-                    <img src="#" id="paypalImg">
                 </div>
                 <!-- Aquí se mostrará el total y el botón de pagar ahora -->
-                <div class="total" id="total">Total:</div>
-                <button type="button" class="btn" id="payNow">Pagar ahora</button>
+                    <div class="totalContainer">
+                        <div class="total" id="total"><h2>Total</h2><p>(iva incluido)</p></div>
+                        <div class="priceAndButton">
+                            <div class="totalPrice" id="totalPrice"><p>6,00 €</p></div>
+                            <button type="button" class="btn" id="payNow">Pagar ahora</button>
+                        </div>
+                    </div>
             </div>
         </form>
     </section>
