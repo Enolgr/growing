ce873438f37824ae442b6e0369dfb3641fbd6b90
Enol <enolgr@gmail.com>
2025-02-05 09:02:32 +0100

    arreglo perfil usuario

--- includes/opiniones.php
+++ includes/opiniones.php
@@ -1,7 +1,8 @@
 <?php echo '
   <section class="contenedor-opiniones">
+  <div class="colapse-titulo-opiniones">
     <h1 class="titulo-opiniones">¿Qué opinan sobre nuestros cursos?</h1>
-
+</div>
     <!-- Slider: el contenido se generará desde JS -->
     <div class="slider-wrapper">
       <div class="slider" id="slider">
