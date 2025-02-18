e5c4e89bec40200e9e2a3e8f816509a91cd413e8
Enol <enolgr@gmail.com>
2025-02-18 19:27:48 +0100

    cambio en cursos img

--- cursos.php
+++ cursos.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
     <title>Cursos</title>
     <!-- Normalize & CSS -->
     <link rel="stylesheet" href="./styles/normalize.css">
@@ -19,7 +20,10 @@
     <section class="cursos">
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <div class="contenedor-hover">
+                    <img src="./img/images/crop.webp" alt="curso palas">
+                    <div class="overlay"></div>
+                </div>
             </div>
             <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
@@ -27,7 +31,7 @@
                 Blanditiis eligendi quaerat excepturi?</p>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
-              <!-- Redirigir a la pagina de detalles del curso -->
+                <!-- Redirigir a la página de detalles del curso -->
                 <a href="./detalles-curso.php"><button class="btn" id="btn">Ver curso</button></a>
                 <img src="./img/icons/Fav.svg" alt="favoritos" class="estrella">
             </div>
@@ -35,7 +39,10 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <div class="contenedor-hover">
+                    <img src="./img/images/crop.webp" alt="curso palas">
+                    <div class="overlay"></div>
+                </div>
             </div>
             <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
@@ -43,7 +50,6 @@
                 Blanditiis eligendi quaerat excepturi?</p>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
-              <!-- Redirigir a la pagina de detalles del curso -->
                 <a href="./detalles-curso.php"><button class="btn" id="btn">Ver curso</button></a>
                 <img src="./img/icons/Fav.svg" alt="favoritos" class="estrella">
             </div>
@@ -51,15 +57,17 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <div class="contenedor-hover">
+                    <img src="./img/images/crop.webp" alt="curso palas">
+                    <div class="overlay"></div>
+                </div>
             </div>
             <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
-                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
-                Blanditiis eligendi quaerat excepturi?</p>
+                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid ejercicio
+                exercitationem. Blanditiis eligendi quaerat excepturi?</p>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
-              <!-- Redirigir a la pagina de detalles del curso -->
                 <a href="./detalles-curso.php"><button class="btn" id="btn">Ver curso</button></a>
                 <img src="./img/icons/Fav.svg" alt="favoritos" class="estrella">
             </div>
@@ -67,15 +75,17 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <div class="contenedor-hover">
+                    <img src="./img/images/crop.webp" alt="curso palas">
+                    <div class="overlay"></div>
+                </div>
             </div>
             <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
-                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
-                Blanditiis eligendi quaerat excepturi?</p>
+                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid ejercicio
+                exercitationem. Blanditiis eligendi quaerat excepturi?</p>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
-              <!-- Redirigir a la pagina de detalles del curso -->
                 <a href="./detalles-curso.php"><button class="btn" id="btn">Ver curso</button></a>
                 <img src="./img/icons/Fav.svg" alt="favoritos" class="estrella">
             </div>
@@ -83,15 +93,17 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <div class="contenedor-hover">
+                    <img src="./img/images/crop.webp" alt="curso palas">
+                    <div class="overlay"></div>
+                </div>
             </div>
             <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
-                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
-                Blanditiis eligendi quaerat excepturi?</p>
+                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid ejercicio
+                exercitationem. Blanditiis eligendi quaerat excepturi?</p>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
-              <!-- Redirigir a la pagina de detalles del curso -->
                 <a href="./detalles-curso.php"><button class="btn" id="btn">Ver curso</button></a>
                 <img src="./img/icons/Fav.svg" alt="favoritos" class="estrella">
             </div>
@@ -99,39 +111,41 @@
 
         <div class="detalles-curso">
             <div class="img-curso">
-                <img src="./img/images/pexels-cottonbro-4503269.webp" alt="curso palas">
+                <div class="contenedor-hover">
+                    <img src="./img/images/crop.webp" alt="curso palas">
+                    <div class="overlay"></div>
+                </div>
             </div>
             <h2 class="nombre-curso">Titulo curso</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure laboriosam totam beatae, dignissimos
-                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid exercitationem.
-                Blanditiis eligendi quaerat excepturi?</p>
+                consequatur odit? Eaque officiis quam illo aut velit. Nemo cumque obcaecati aliquid ejercicio
+                exercitationem. Blanditiis eligendi quaerat excepturi?</p>
             <!-- Icono para añadir a favoritos -->
             <div class="icons">
-              <!-- Redirigir a la pagina de detalles del curso -->
                 <a href="./detalles-curso.php"><button class="btn" id="btn">Ver curso</button></a>
                 <img src="./img/icons/Fav.svg" alt="favoritos" class="estrella">
             </div>
         </div>
-
     </section>
 
     <div class="paginacion">
-            <button class="pagina">1</button>
-            <button class="pagina">2</button>
-            <button class="pagina">3</button>
-            <button class="pagina">4</button>
-            <button class="pagina">5</button>
-            <button class="pagina">...</button>
-            <button class="pagina">10</button>
+        <button class="pagina">1</button>
+        <button class="pagina">2</button>
+        <button class="pagina">3</button>
+        <button class="pagina">4</button>
+        <button class="pagina">5</button>
+        <button class="pagina">...</button>
+        <button class="pagina">10</button>
     </div>
 
-    <!-- Aqui iria la paginación de los cursos  -->
     <!-- Newsletter -->
     <?php include_once "./includes/newsletter.php"; ?>
 
     <!-- Marquee & Footer -->
-    <?php include_once "./includes/marquee.php";
-    include_once "./includes/footer.php"; ?>
+    <?php 
+        include_once "./includes/marquee.php";
+        include_once "./includes/footer.php"; 
+    ?>
 
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="./scripts/estrellas.js"></script>
