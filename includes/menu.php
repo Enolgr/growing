e3fec1c3ee9ece74c63a3e6e4785ae97075b7eb7
Enol <enolgr@gmail.com>
2025-02-18 13:51:24 +0100

    circulos en el menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,20 +1,25 @@
 <?php
 echo '
-<header id="menu"><a href="./index.php">
-<div class="logo-menu"><img src="./img/logo.svg" alt="logo-growing"></div></a>
-<div class="items-menu">
-    <a href="./cursos.php">Cursos</a>
-    <a href="./tienda.php">Tienda</a>
-    <a href="./tarifas.php">Tarifas</a>
-    <span class="anchor-bg"><a href="./top.php">Lo más popular</a></span>
-    <div class="icons-nav">
-    <a href="./carrito-compra.php">
-    <div class="iconos-menu">
-        <img src="./img/icons/Carrito.svg" alt="carrito"></a>
-        <a href="./perfil-usuario.php"><img src="./img/icons/Perfil.svg" alt="perfil"></a>
-        </div>
+<header id="menu">
+  <a href="./index.php">
+    <div class="logo-menu">
+      <img src="./img/logo.svg" alt="logo-growing">
     </div>
-</div>
-</header>'
+  </a>
+  <div class="items-menu">
+    <a class="circle" href="./cursos.php">Cursos</a>
+    <a class="circle" href="./tienda.php">Tienda</a>
+    <a class="circle" href="./tarifas.php">Tarifas</a>
+    <span class="anchor-bg">
+      <a href="./top.php">Lo más popular</a>
+    </span>
+    <div class="icons-nav">
+      <a href="./carrito-compra.php">
+        <div class="iconos-menu carrito"></div>
+      </a>
+      <div class="iconos-menu perfil"></div>
+    </div>
+  </div>
+</header>
+';
 ?>
-
