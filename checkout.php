8e45659f7a7b3a00c2097d8196fe00a7747339a8
Enol <enolgr@gmail.com>
2025-02-18 19:44:24 +0100

    cambio en finalizar

--- checkout.php
+++ checkout.php
@@ -14,14 +14,14 @@
     <?php include_once "./includes/menu.php"?>
     <div class="titleArrow">
         <a href="./carrito-compra.php">
-        <img src="./img/icons/back.svg" alt="">
+            <img src="./img/icons/back.svg" alt="">
         </a>
         <h1 class="title2">Finaliza tu compra</h1>
     </div>
-    <section class= "legalContainer">
+    <section class="legalContainer">
         <form action="****.php" method="post">
-            <div class="checkoutCard"><!--Tarjeta con los datos de pago y envío  entera -->
-                <!-- Apartdo datos de entrega -->
+            <div class="checkoutCard"><!--Tarjeta con los datos de pago y envío entera -->
+                <!-- Apartado datos de entrega -->
                 <div class="sendData">
                     <h2>Entrega</h2>
                     <div class="nameLast">
@@ -29,31 +29,31 @@
                         <input type="text" id="name" name="lastName" placeholder="Apellidos" required>
                     </div>
                     <input type="text" id="adress" name="adress" placeholder="Dirección" required>
-                    <input type="text" id="house" name="house" placeholder="Casa,apartamento,etc.(opcional)">
+                    <input type="text" id="house" name="house" placeholder="Casa, apartamento, etc. (opcional)">
                     <div class="directionData">
-                        <input type="text" id="postalCode" name="postalCode" placeholder="Codigo postal" required>
+                        <input type="text" id="postalCode" name="postalCode" placeholder="Código postal" required>
                         <input type="text" id="city" name="city" placeholder="Ciudad" required>
                         <input type="text" id="province" name="province" placeholder="Provincia" required>
                     </div>
                     <input type="tel" id="phone" name="phone" placeholder="Teléfono" required>
                     <br>
                     <div class="check-checkout">
-                        <?php include "./includes/checkbox.php"; ?>
-                        <p>Guardar mi información y pagar más rápido la proxima vez</p>
+                        <?php include "./includes/white-checkbox.php"; ?>
+                        <p>Guardar mi información y pagar más rápido la próxima vez</p>
                     </div>
                 </div>
 
-                <!-- Apartdo  empresa envío -->
+                <!-- Apartado empresa envío -->
                 <div class="deliveryMethod">
                     <hr>
                     <h2>Método de envío</h2>
                     <div class="deliveryData" id="deliveryN1">
-                        <input class="radius" type="radio" id="delivery1" name="delivery" value="glsEcomomy"></input>
+                        <input class="radius" type="radio" id="delivery1" name="delivery" value="glsEcomomy">
                         <label for="delivery1">GLS Economy</label>
                     </div>
                     <p class="subtitle">Entrega en 48-72h</p>
                     <div class="deliveryData" id="deliveryN2">
-                        <input class="radius" type="radio" id="delivery2" name="delivery" value="correosExpress"></input>
+                        <input class="radius" type="radio" id="delivery2" name="delivery" value="correosExpress">
                         <label for="delivery2">Correos Express</label>
                     </div>
                     <p class="subtitle">Entrega en 24h</p>
@@ -64,12 +64,12 @@
                     <h2>Pago</h2>
                     <p>Todas las transacciónes son seguras y están encriptadas</p>
                     <div class="creditCardContainer">
-                            <input class="radius "type="radio" id="creditCard" name="payment"></input>
-                            <label for="bizum">Tarjeta de crédito</label>
-                            <div class="visaMasterLogos">
-                                <img src="./img/logos/visa-classic-svgrepo-com.svg" id="visaPayImg" alt="Pago con Visa">
-                                <img src="./img/logos/mastercard-full-svgrepo-com.svg" id="MasterCardPayImg" alt="Pago con Master Card">
-                            </div>
+                        <input class="radius" type="radio" id="creditCard" name="payment">
+                        <label for="bizum">Tarjeta de crédito</label>
+                        <div class="visaMasterLogos">
+                            <img src="./img/logos/visa-classic-svgrepo-com.svg" id="visaPayImg" alt="Pago con Visa">
+                            <img src="./img/logos/mastercard-full-svgrepo-com.svg" id="MasterCardPayImg" alt="Pago con Master Card">
+                        </div>
                     </div>
                     <div class="creditForm">
                         <input type="text" id="cardNum" placeholder="Número de la tarjeta">
@@ -80,7 +80,7 @@
                         <input type="text" id="ownerName" placeholder="Nombre del titular">
                     </div>
                     <div class="check-checkout">
-                        <?php include "./includes/checkbox.php";?>
+                        <?php include "./includes/white-checkbox.php"; ?>
                         <p>Usar la dirección de envío como la dirección de facturación</p>
                     </div>
                     <div class="otherMethodsContainer">
@@ -90,7 +90,6 @@
                             <div class="imgsPayment">
                                 <img src="./img/logos/Bizum.svg" id="bizumImg" alt="Pago con Bizum">
                             </div>
-                            
                         </div>
                         <div class="paymentOption">
                             <input class="radius" type="radio" id="googleApple" name="payment">
@@ -107,23 +106,29 @@
                                 <img src="./img/logos/paypal.svg" id="paypalImg" alt="Pago con PayPal">
                             </div>
                         </div>
-                </div>
-                <hr>
-                <!-- Aquí se mostrará el total y el botón de pagar ahora -->
+                    </div>
+                    <hr>
+                    <!-- Aquí se mostrará el total y el botón de pagar ahora -->
                     <div class="totalContainer">
-                        <div class="total" id="total"><h2>Total</h2><p>(iva incluido)</p></div>
+                        <div class="total" id="total">
+                            <h2>Total</h2>
+                            <p>(iva incluido)</p>
+                        </div>
                         <div class="priceAndButton">
-                            <div class="totalPrice" id="totalPrice"><p>6,00 €</p></div>
+                            <div class="totalPrice" id="totalPrice">
+                                <p>6,00 €</p>
+                            </div>
                             <button type="button" class="btn" id="payNow">Pagar ahora</button>
                         </div>
                     </div>
+                </div>
             </div>
         </form>
     </section>
     <section class="footer">
-    <!-- Marquee & Footer -->
-    <?php include_once "./includes/marquee.php"; ?>
-    <?php include_once "./includes/footer.php"; ?>
+        <!-- Marquee & Footer -->
+        <?php include_once "./includes/marquee.php"; ?>
+        <?php include_once "./includes/footer.php"; ?>
     </section>
 </body>
 </html>
